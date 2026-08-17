ba250124adff6fe395fde08a54e40939302c2133
Daniel Osawaru <35234154+dosawaru@users.noreply.github.com>
2026-08-17 11:50:52 -0700

    create async connection pool and ORM base

---
+++ config/database.py
@@ -0,0 +1,33 @@
+import os 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+# get database URL
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# create engine
+# pool_size: maximum number of connections in the pool
+# max_overflow: maximum number of connections to allow beyond the pool size
+engine = create_engine(
+    DATABASE_URL,
+    pool_size= 10
+    max_overflow= 10,
+    echo= True
+)
+
+# create session
+Session = sessionmaker(bind= engine, autocommit= False, autoflush= False)
+
+# create base class for all models for them to inherit from and be able to use the session
+Base = declarative_base()
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
